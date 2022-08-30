16e3667c2ccbaa4b8939485ce435c35b771fa5cd
9998236414 <karavadararamde74@gmail.com>
2022-08-30 19:53:35 +0530

    crud operations

---
+++ crud/core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import Home, Add_Student, Delete_Student, Edit_Student
+urlpatterns=[
+    path('',Home.as_view(),name='home'),
+    path('add_student/',Add_Student.as_view(),name='add_student'),
+    path('delete_student/',Delete_Student.as_view(),name='delete_student'),
+    path('edit_student/<int:id>',Edit_Student.as_view(),name='edit_student'),
+]
---
+++ crud/core/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render,redirect
+from .models import Student
+from .form import AddStudentForm
+from django.views import View
+# Create your views here.
+
+class Home(View):
+    def get(self,request):
+        stu_data = Student.objects.all()
+        return render(request, 'home.html',{'stu_data':stu_data})
+
+class Add_Student(View):
+    def get(self,request):
+            fm = AddStudentForm()
+            return render(request, 'add_student.html', {'form': fm})
+
+
+    def post(self,request):
+        fm = AddStudentForm(request.POST)
+        if fm.is_valid():
+                fm.save()
+                return redirect('/')
+        else:
+            return render(request, 'add_student.html', {'form': fm})
+
+
+class Delete_Student(View):
+    def post(self,request):
+            data = request.POST
+            id = data.get('id')
+            studata=Student.objects.get(id=id)
+            studata.delete()
+            return redirect('/')
+
+class Edit_Student(View):
+    def get(self,request,id):
+        stu = Student.objects.get(id=id)
+        fm = AddStudentForm(instance=stu)
+        return render(request, 'edit_student.html',{'form': fm})
+
+    def post(self,request,id):
+        stu = Student.objects.get(id=id)
+        fm = AddStudentForm(request.POST,instance=stu)
+        if fm.is_valid():
+                fm.save()
+                return redirect('/')
+
+
+
